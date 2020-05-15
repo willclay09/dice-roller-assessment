530d418d892ef05eafd3e217fabc66c24f6eedd1
William <william_6426@hotmail.com>
2020-05-15 16:57:43 -0500

    structured

---
+++ assets/main.js
@@ -0,0 +1,31 @@
+let dierolls = []
+let rollButton = document.querySelector("#dice-role")
+let totalRolls = document.querySelector("#number-of-rolls")
+let totalOutcome = document.querySelector("#sum-of-rolls")
+let diceNumber = document.querySelector("#dice")
+let DisplayRolls = document.querySelector("#dice-number")
+let index = 0
+let diceindex = 0
+
+rollButton.addEventListener('click', function () {
+    let TotalSum = 0
+    let totalDice = diceNumber.value
+while (index < totalDice) {
+    let randomNUmber = Math.floor((Math.random() * 6) + 1)
+    dierolls.push(randomNUmber)
+   TotalSum += randomNUmber
+    index += 1
+}
+totalOutcome.innerHTML = "The Sum Of All Rolls " + TotalSum
+})
+    console.log(index)
+totalRolls.addEventListener('click', function(){
+    while (diceindex < dierolls.length) {
+      let cup = document.createElement('li')
+        cup.innerHTML = dierolls[diceindex]
+    DisplayRolls.appendChild(cup)
+        diceindex += 1
+    }
+ //lookup documnet.create element and element.appendchild
+})
+console.log(diceindex)
